a82b4588e25339e5227b3817f3647c6e8db3f65a
3nt3 <gott@3nt3.de>
2026-01-07 02:02:34 +0100

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import OpenHardwareLogo from "$lib/assets/openhardware.svg";
+</script>
+
+<main
+	class="bg-pink-300 w-full h-screen py-12 px-32 flex flex-col justify-between"
+>
+	<nav class="flex justify-center">
+		<ul class="flex gap-16 text-3xl text-white font-bold font-display">
+			<li>
+				<a href="https://matrix.to/#/!jMcHN4pivoMm5HuzcF:3nt3.de"
+					>Matrix</a
+				>
+			</li>
+			<li>
+				<a href="https://github.com/3nt3/open-vaporphase">Git</a>
+			</li>
+			<li>
+				<a href="https://draw.openvaporphase.lol">Excalidraw</a>
+			</li>
+		</ul>
+	</nav>
+	<div class="mb-32">
+		<h1 class="text-[90pt] text-white font-bold leading-12 font-display">
+			Open <OpenHardwareLogo class="inline mb-[90pt] -ml-10"/> <br />
+			Vaporphase
+		</h1>
+		<h2 class="text-4xl text-white font-bold mt-16 font-body">Work in progress.</h2>
+	</div>
+</main>

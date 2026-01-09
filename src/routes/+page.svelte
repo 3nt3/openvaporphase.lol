9d949471345322d3221a63d8aca970288d46b4fe
3nt3 <gott@3nt3.de>
2026-01-09 02:12:45 +0100

    Add md link

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,25 +3,28 @@
 </script>
 
 <main
-	class="bg-pink-300 w-full h-screen py-12 px-12 lg:px-32 flex flex-col justify-between"
+	class="bg-pink-300 w-full h-screen py-12 px-6 md:px-32 flex flex-col justify-between"
 >
-	<nav class="flex justify-center">
-		<ul class="flex gap-16 text-3xl text-white font-bold font-display">
+	<nav class="flex md:justify-center">
+		<ul class="flex md:flex-row md:gap-16 gap-8 text-3xl text-white font-bold font-display">
 			<li>
 				<a href="https://matrix.to/#/!jMcHN4pivoMm5HuzcF:3nt3.de"
-					>Matrix</a
+					>matrix</a
 				>
 			</li>
 			<li>
-				<a href="https://github.com/3nt3/open-vaporphase">Git</a>
+				<a href="https://github.com/3nt3/open-vaporphase">git</a>
 			</li>
 			<li>
-				<a href="https://draw.openvaporphase.lol">Excalidraw</a>
+				<a href="https://md.openvaporphase.lol">md</a>
+			</li>
+			<li>
+				<a href="https://draw.openvaporphase.lol">draw</a>
 			</li>
 		</ul>
 	</nav>
 	<div class="mb-32">
-		<h1 class="lg:text-[90pt] text-7xl text-white font-bold lg:leading-12 leading-0 font-display">
+		<h1 class="lg:text-[90pt] text-6xl text-white font-bold lg:leading-12 leading-0 font-display">
 			Open <OpenHardwareLogo class="inline mb-[90pt] -ml-10"/> <br />
 			Vaporphase
 		</h1>

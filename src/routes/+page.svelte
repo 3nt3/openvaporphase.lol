d102c5beca9eaa7a6d9655f3986d78028935e570
3nt3 <gott@3nt3.de>
2026-01-15 14:51:17 +0100

    fix git link
    Signed-off-by: 3nt3 <gott@3nt3.de>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,14 +6,18 @@
 	class="bg-pink-300 w-full h-screen py-12 px-6 md:px-32 flex flex-col justify-between"
 >
 	<nav class="flex md:justify-center">
-		<ul class="flex md:flex-row md:gap-16 gap-8 text-3xl text-white font-bold font-display">
+		<ul
+			class="flex md:flex-row md:gap-16 gap-8 text-3xl text-white font-bold font-display"
+		>
 			<li>
 				<a href="https://matrix.to/#/!jMcHN4pivoMm5HuzcF:3nt3.de"
 					>matrix</a
 				>
 			</li>
 			<li>
-				<a href="https://github.com/3nt3/open-vaporphase">git</a>
+				<a href="https://github.com/open-vaporphase/open-vaporphase"
+					>git</a
+				>
 			</li>
 			<li>
 				<a href="https://md.openvaporphase.lol">md</a>
@@ -24,10 +28,14 @@
 		</ul>
 	</nav>
 	<div class="mb-32">
-		<h1 class="lg:text-[90pt] text-6xl text-white font-bold lg:leading-12 leading-0 font-display">
-			Open <OpenHardwareLogo class="inline mb-[90pt] -ml-10"/> <br />
+		<h1
+			class="lg:text-[90pt] text-6xl text-white font-bold lg:leading-12 leading-0 font-display"
+		>
+			Open <OpenHardwareLogo class="inline mb-[90pt] -ml-10" /> <br />
 			Vaporphase
 		</h1>
-		<h2 class="text-4xl text-white font-bold mt-16 font-body">Work in progress.</h2>
+		<h2 class="text-4xl text-white font-bold mt-16 font-body">
+			Work in progress.
+		</h2>
 	</div>
 </main>
